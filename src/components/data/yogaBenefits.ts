df457fc7dc46b88136cc577dd4d4ec8896f1e406
Milena <milena.livian@gmail.com>
2025-01-08 11:43:31 +0100

    wip

--- src/components/data/yogaBenefits.ts
+++ src/components/data/yogaBenefits.ts
@@ -13,20 +13,20 @@ export const YogaBenefits: YogaBenefit[] = [
     icon: HeartPulse,
     title: "Improves Physical Health",
     details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non  hendrerit felis. Duis a sodales diam. Nunc pellentesque lorem erat, in  scelerisque dui consectetur at. Morbi vel ornare nisl. Nulla ut ex ac  elit imperdiet convallis. Praesent elementum mi in enim mattis commodo.",
+      "1111111111 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non  hendrerit felis. Duis a sodales diam. Nunc pellentesque lorem erat, in  scelerisque dui consectetur at. Morbi vel ornare nisl. Nulla ut ex ac  elit imperdiet convallis. Praesent elementum mi in enim mattis commodo.",
   },
   {
     id: 2,
     icon: ZapOff,
     title: "Reduce Stress",
     details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non  hendrerit felis. Duis a sodales diam. Nunc pellentesque lorem erat, in  scelerisque dui consectetur at. Morbi vel ornare nisl. Nulla ut ex ac  elit imperdiet convallis. Praesent elementum mi in enim mattis commodo.",
+      "2222222222222 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non  hendrerit felis. Duis a sodales diam. Nunc pellentesque lorem erat, in  scelerisque dui consectetur at. Morbi vel ornare nisl. Nulla ut ex ac  elit imperdiet convallis. Praesent elementum mi in enim mattis commodo.",
   },
   {
     id: 3,
     icon: Smile,
     title: "Promotes Emotional Well-Being",
     details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non  hendrerit felis. Duis a sodales diam. Nunc pellentesque lorem erat, in  scelerisque dui consectetur at. Morbi vel ornare nisl. Nulla ut ex ac  elit imperdiet convallis. Praesent elementum mi in enim mattis commodo.",
+      "3333333333333 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non  hendrerit felis. Duis a sodales diam. Nunc pellentesque lorem erat, in  scelerisque dui consectetur at. Morbi vel ornare nisl. Nulla ut ex ac  elit imperdiet convallis. Praesent elementum mi in enim mattis commodo.",
   },
 ];
