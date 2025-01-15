a33616e6e0ed880807f74a7a090172eda03d170b
Milena <milena.livian@gmail.com>
2025-01-15 15:36:28 +0100

    fix

--- src/components/api/getVideos/videoService.ts
+++ src/components/api/getVideos/videoService.ts
@@ -1,7 +1,7 @@
 import { SelectionFilter } from "@/components/models/selection";
 import { VideoPreview } from "@/components/models/videoPreview";
 import { mapVideoPreviewDtoToVideoPreview } from "./videosMapping";
-import { fetchVideos } from "./videoQuery";
+import { fetchVideos } from "./videoFetch";
 
 export async function GetVideos(
   selection: SelectionFilter
